e18bf964a865d45427ee3b49174c4f531e421d8f
Dimitro83 <126614027+Dimitro83@users.noreply.github.com>
2023-03-13 00:43:13 +0200

    DZ_09

---
+++ DZ_08_02.py
@@ -0,0 +1,11 @@
+while True:
+    number = input('To check "Even" or "Odd" or "Zero", please enter a number: ')
+    if not number.isdigit():
+        continue
+    else:
+        break
+
+
+result = lambda number: print(f'Number {number} is ZERO') if int(number) == 0 else print(f'Number {number} is EVEN') \
+    if int(number) % 2 == 0 else print(f'Number {number} is ODD')
+result(number)
